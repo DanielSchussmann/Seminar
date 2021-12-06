85e1e1718b04942ed92eb4738c3a1f181283d93c
Daniel <liewanx@gmail.com>
2021-12-06 21:36:22 +0100

    ...

---
+++ Check_profit.py
@@ -0,0 +1,159 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+
+
+
+all = pd.read_csv("AUDUSD_20.11_1Y_1H.csv", usecols=[1,2,3,4])
+dt= all.to_numpy().copy()
+dt =dt[50:-1]
+from matplotlib.animation import FuncAnimation, PillowWriter
+
+matplotlib.style.use('bmh')
+fig = plt.figure()
+ax = fig.add_subplot()
+ax.set_xlim(0.001,50)
+ax.set_ylim(0.725,0.75)
+ax.set_xlabel('time')
+ax.set_ylabel('Price')
+ax.set_title('AI-visualization')
+
+#fig.set_size_inches(20, 20)
+#ax.text([0, 0], 'boxed italics text in data coords', style='italic')
+
+short_c = '#D13838'
+long_c = '#63ED7C'
+
+def animate(i=30):
+    #ax.annotate(str("egg"), xy=(0, 0))
+    if i%10==0:
+        ax.plot([i,i+4],[dt[i][0]-0.001,dt[i][0]-0.001],color="coral",linewidth=1,label="Take Profit",linestyle="dashed")#stoploss
+        ax.plot([i, i + 4], [dt[i][3]+0.001, dt[i][3]+0.001], color="cyan", label="Stop Loss",linewidth=1,linestyle="dashed")  # stoploss
+    #Drawing candles block
+    if dt[i][0]>dt[i][3]:
+        ax.plot([i,i], [dt[i][1], dt[i][2]], color=long_c, linewidth=1,)
+        ax.plot([i, i], [dt[i][0], dt[i][3]], color=long_c, linewidth=5, )
+    else:
+        ax.plot([i, i], [dt[i][1], dt[i][2]], color=short_c, linewidth=1, )
+        ax.plot([i, i], [dt[i][0], dt[i][3]], color=short_c, linewidth=5, )
+
+anim = FuncAnimation(fig, animate, interval=20000,frames=50)#frames = i values
+anim.save("tmp/movie.gif", writer=PillowWriter(fps=1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+def draw_candles(ax,data):
+    short_c='#D13838'
+    long_c='#63ED7C'
+    i=0
+    j=0
+    while i<len(data):
+        if data[j][0]>data[j][3]:
+            ax.plot([i,i],[data[j][1], data[j][2]], color=short_c, linewidth=1,) #wick
+            ax.plot([i,i],[data[j][0], data[j][3]], color=short_c, linewidth=5,) #body
+        else:
+            ax.plot([i, i], [data[j][1], data[j][2]], color=long_c, linewidth=1, )  # wick
+            ax.plot([i, i], [data[j][0], data[j][3]], color=long_c, linewidth=5, )  # body
+        i+=1
+        j+=1
+
+    ax.margins(x=0)
+    fig.set_size_inches(len(data)*0.2, len(data)*0.1)
+    #plt.show()
+    plt.savefig("tmp/test_1.png",bbox_inches='tight')
+
+
+
+"""
+# First set up the figure, the axis, and the plot element we want to animate
+fig = plt.figure()
+ax = plt.axes(xlim=(0, 2), ylim=(-2, 2))
+line, = ax.plot([], [], lw=2)
+
+# initialization function: plot the background of each frame
+def init():
+    line.set_data([], [])
+    return line,
+
+# animation function.  This is called sequentially
+def animate(i):
+    x = np.linspace(0, 2, 1000)
+    y = np.sin(2 * np.pi * (x - 0.01 * i))
+    line.set_data(x, y)
+    return line,
+
+# call the animator.  blit=True means only re-draw the parts that have changed.
+anim = animation.FuncAnimation(fig, animate, init_func=init,
+                               frames=200, interval=20, blit=True)
+anim.save('tmp/basic_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])"""
+
+
+
+
+
+"""def draw_candle(data,i,color):
+    if data[0] > data[3]:
+        ax.plot([i, i], [data[1], data[2]], color=short_c, linewidth=1, )  # wick
+        ax.plot([i, i], [data[0], data[3]], color=short_c, linewidth=5, )  # body
+    else:
+        ax.plot([i, i], [data[1], data[2]], color=long_c, linewidth=1, )  # wick
+        ax.plot([i, i], [data[0], data[3]], color=long_c, linewidth=5, )  # body"""
+
+
+
+"""def draw_candles(data):
+    short_c='#D13838'
+    long_c='#63ED7C'
+    matplotlib.style.use('fivethirtyeight')
+    fig, ax = plt.subplots()
+    i=0
+    j=0
+    while i<len(data):
+        if data[j][0]>data[j][3]:
+            ax.plot([i,i],[data[j][1], data[j][2]], color=short_c, linewidth=1,) #wick
+            ax.plot([i,i],[data[j][0], data[j][3]], color=short_c, linewidth=5,) #body
+        else:
+            ax.plot([i, i], [data[j][1], data[j][2]], color=long_c, linewidth=1, )  # wick
+            ax.plot([i, i], [data[j][0], data[j][3]], color=long_c, linewidth=5, )  # body
+        i+=1
+        j+=1
+
+    ax.margins(x=0)
+    fig.set_size_inches(len(data)*0.2, len(data)*0.1)
+    #plt.show()
+    plt.savefig("tmp/test_1.png",bbox_inches='tight')
+"""
+#draw_candles(dt[100:210])
+
+
+
+
+#df=np.array(np.array_split(df,836)).reshape([836,10])
+#['Solarize_Light2', '_classic_test_patch', '_mpl-gallery', '_mpl-gallery-nogrid', 'bmh', 'classic', 'dark_background', 'fast', 'fivethirtyeight', 'ggplot', 'grayscale', 'seaborn', 'seaborn-bright', 'seaborn-colorblind', 'seaborn-dark', 'seaborn-dark-palette', 'seaborn-darkgrid', 'seaborn-deep', 'seaborn-muted', 'seaborn-notebook', 'seaborn-paper', 'seaborn-pastel', 'seaborn-poster', 'seaborn-talk', 'seaborn-ticks', 'seaborn-white', 'seaborn-whitegrid', 'tableau-colorblind10']
+
+
+def draw_dis(data,tp,sl):
+    matplotlib.style.use('bmh')
+    fig, ax = plt.subplots()
+    ax.axhline(y=tp,color='g',label='Take-Profit',linewidth=1,linestyle='dashed')
+    ax.axhline(y=sl,color='r',label="Stop-Soss",linewidth=1,linestyle='dashed')
+    ax.axhline(y=data[0][0], color='0',linestyle='dashed',label="Starting price",linewidth=1)
+    ax.plot(data,label="Price move",color="coral")
+    rect = matplotlib.patches.Rectangle((1,data[0][0]), 10,tp-data[0][3],linestyle='dashed', alpha=0.5,linewidth=1, edgecolor='g', facecolor='g')
+
+    ax.legend()
+    ax.add_patch(rect)
+    plt.show()
+
+#draw_dis(egg,0.7310,0.7290)
--- main.py
+++ main.py
@@ -2,62 +2,97 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 import matplotlib.pyplot as plt
-from keras.utils.vis_utils import model_to_dot
-
 
 
 # read csv data, drop date coz not important
-df_numpied = np.array(np.array_split((pd.read_csv("AUDUSD_20.11_1Y_1H.csv", usecols=[1,2,3,4])).to_numpy(),418))# uses only the middle colums and gets split into subsets of 20 (8360/20 = 418)
+#df_numpied = np.array(np.array_split((pd.read_csv("AUDUSD_20.11_1Y_1H.csv", usecols=[1,2,3,4],columns=["Open","High","Low","Close"])).to_numpy(),418))# uses only the middle colums and gets split into subsets of 20 (8360/20 = 418)
+close = pd.read_csv("AUDUSD_20.11_1Y_1H.csv", usecols=[4])
+lbls = close.columns[0]
+df = close.copy()
+df=np.array(np.array_split(df,836)).reshape([836,10])
+
+
+
+x_train=df[::2]
+y_train=np.zeros([len(x_train),len(x_train[0])])
+x_val=df[1::2]#all the even indicies
+y_val=np.zeros([len(x_val),len(x_val[0])])
+print(x_train[0])
+print(y_train[0])
+
+plt.plot(df)
+plt.show()
+
+"""
 
 train=[]
 test=[]
-
 for i in range(0,len(df_numpied)):#  the shape 20 arrays into two, normalize and append to array.
     a,b=np.array_split(df_numpied[i],2)
-    b=np.reshape(b,(1,40))# reshapes the current array to be a matrix(4x10) instead of a collection of vectors
-    a=np.reshape(a,(1,40))
-
+    b=b.reshape(1,40).astype("float32")# reshapes the current array to be a matrix(4x10) instead of a collection of vectors
+    a=a.reshape(1,40).astype("float32")
     train.append(tf.keras.utils.normalize(a))# pushes the normalized current array
-
     test.append(np.array([b[0][0],max(b[0]),min(b[0])]))# pushes the first, the max and the min of the current array
 
-#train=tf.constant(train)# tensorfy the training data
-#test=tf.constant(test)
-
-def show_me_the_money(x):
-    plt.plot([0,10],[test[x][0],test[x][1]],color='green')
-    plt.plot([0,10],[test[x][0],test[x][2]],color='red')
-
-    plt.text(10, test[x][1], str(test[x][1]), color='green')
-    plt.text(10, test[x][2], str(test[x][2]),color='red')
-    plt.text(0, test[x][0], str(test[x][0]), color='gray')
-    #plt.text()
-    plt.show()
-
-show_me_the_money(21)
 
 
 
-model = tf.keras.Sequential()
-model.add(tf.keras.layers.Input(shape=(40,)))
-model.add(tf.keras.layers.Dense(64, activation="relu"))
-model.add(tf.keras.layers.Dense(64, activation="relu"))
-model.add(tf.keras.layers.Dense(2,activation="softmax"))
+
+
+train=np.reshape(train,(len(train),40)).astype("float32")
+
+x_train=tf.constant(train)# tensorfy the training data
+y_train=np.zeros([len(x_train),1]).astype("float32")#.reshape((-1, 1))
+
+x_val=tf.constant(test)
+y_val=np.zeros([len(x_val),1]).astype("float32")
+"""
+
+
+
+
+
+
+
+inputs = tf.keras.Input(shape=(10,), name="digits")
+x = tf.keras.layers.Dense(64, activation="relu", name="dense_1")(inputs)
+x = tf.keras.layers.Dense(64, activation="relu", name="dense_2")(x)
+outputs = tf.keras.layers.Dense(10, activation="softmax", name="predictions")(x)
+model = tf.keras.Model(inputs=inputs, outputs=outputs)
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-
 tf.keras.utils.plot_model(model, to_file='tmp/NN_1_visual.png', show_shapes=True)# Draw model
 print(model.summary())
 
-print("Fit model on training data")
-history = model.fit(
-    train,
-    train,
+model.fit(
+    x_train,y_train,
     batch_size=20,
-    epochs=2,
+    epochs=20,
     # We pass some validation for
     # monitoring validation loss and metrics
     # at the end of each epoch
-    validation_data=(test, test),)
+    validation_data=(x_val,y_val))
+
+accuracy = model.evaluate(x_train, x_val,verbose=0)
+print(accuracy)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -75,9 +110,26 @@ def prep_data(input,cut):
         test_f.append(np.array([b[0][0], max(b[0]), min(b[0])]))
     return [tf.constant(train_f),tf.constant(test_f)]
 """
+"""def show_me_the_money(x):
+    plt.plot([0,10],[test[x][0],test[x][1]],color='green')
+    plt.plot([0,10],[test[x][0],test[x][2]],color='red')
+    plt.text(10, test[x][1], str(test[x][1]), color='green')
+    plt.text(10, test[x][2], str(test[x][2]),color='red')
+    plt.text(0, test[x][0], str(test[x][0]), color='gray')
+    #plt.text()
+    plt.show()
+show_me_the_money(25)
+"""
+"""
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Input(shape=(40,)))
+model.add(tf.keras.layers.Dense(64, activation="relu"))
+model.add(tf.keras.layers.Dense(64, activation="relu"))
+model.add(tf.keras.layers.Dense(3,activation ="relu"))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.build()
+"""
 
-
-#print(train[0])
 
 
 
