87960c1abcfaad7440ec722133e5855447aaf481
Daniel <liewanx@gmail.com>
2021-12-07 11:17:03 +0100

    ...

--- Check_profit.py
+++ Check_profit.py
@@ -20,7 +20,7 @@ long_c = '#63ED7C'
 
 
 def animate(i=30):
-    if i%10==0:
+    if i%11==0:
         ax.plot([i,i+4],[dt[i][0]-0.001,dt[i][0]-0.001],color="coral",linewidth=1,linestyle="dashed")
         ax.plot([i, i + 4], [dt[i][3]+0.001, dt[i][3]+0.001], color="cyan",linewidth=1,linestyle="dashed")
     if dt[i][0]>dt[i][3]:
@@ -40,71 +40,7 @@ anim.save("tmp/movie.gif", writer=PillowWriter(fps=1))
 
 
 
-
-
-
-
-
-def draw_candles(ax,data):
-    short_c='#D13838'
-    long_c='#63ED7C'
-    i=0
-    j=0
-    while i<len(data):
-        if data[j][0]>data[j][3]:
-            ax.plot([i,i],[data[j][1], data[j][2]], color=short_c, linewidth=1,) #wick
-            ax.plot([i,i],[data[j][0], data[j][3]], color=short_c, linewidth=5,) #body
-        else:
-            ax.plot([i, i], [data[j][1], data[j][2]], color=long_c, linewidth=1, )  # wick
-            ax.plot([i, i], [data[j][0], data[j][3]], color=long_c, linewidth=5, )  # body
-        i+=1
-        j+=1
-
-    ax.margins(x=0)
-    fig.set_size_inches(len(data)*0.2, len(data)*0.1)
-    #plt.show()
-    plt.savefig("tmp/test_1.png",bbox_inches='tight')
-
-
-
-"""
-# First set up the figure, the axis, and the plot element we want to animate
-fig = plt.figure()
-ax = plt.axes(xlim=(0, 2), ylim=(-2, 2))
-line, = ax.plot([], [], lw=2)
-
-# initialization function: plot the background of each frame
-def init():
-    line.set_data([], [])
-    return line,
-
-# animation function.  This is called sequentially
-def animate(i):
-    x = np.linspace(0, 2, 1000)
-    y = np.sin(2 * np.pi * (x - 0.01 * i))
-    line.set_data(x, y)
-    return line,
-
-# call the animator.  blit=True means only re-draw the parts that have changed.
-anim = animation.FuncAnimation(fig, animate, init_func=init,
-                               frames=200, interval=20, blit=True)
-anim.save('tmp/basic_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])"""
-
-
-
-
-
-"""def draw_candle(data,i,color):
-    if data[0] > data[3]:
-        ax.plot([i, i], [data[1], data[2]], color=short_c, linewidth=1, )  # wick
-        ax.plot([i, i], [data[0], data[3]], color=short_c, linewidth=5, )  # body
-    else:
-        ax.plot([i, i], [data[1], data[2]], color=long_c, linewidth=1, )  # wick
-        ax.plot([i, i], [data[0], data[3]], color=long_c, linewidth=5, )  # body"""
-
-
-
-"""def draw_candles(data):
+def draw_candles(data):
     short_c='#D13838'
     long_c='#63ED7C'
     matplotlib.style.use('fivethirtyeight')
@@ -125,15 +61,11 @@ anim.save('tmp/basic_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])"
     fig.set_size_inches(len(data)*0.2, len(data)*0.1)
     #plt.show()
     plt.savefig("tmp/test_1.png",bbox_inches='tight')
-"""
 #draw_candles(dt[100:210])
 
 
 
-
-#df=np.array(np.array_split(df,836)).reshape([836,10])
 #['Solarize_Light2', '_classic_test_patch', '_mpl-gallery', '_mpl-gallery-nogrid', 'bmh', 'classic', 'dark_background', 'fast', 'fivethirtyeight', 'ggplot', 'grayscale', 'seaborn', 'seaborn-bright', 'seaborn-colorblind', 'seaborn-dark', 'seaborn-dark-palette', 'seaborn-darkgrid', 'seaborn-deep', 'seaborn-muted', 'seaborn-notebook', 'seaborn-paper', 'seaborn-pastel', 'seaborn-poster', 'seaborn-talk', 'seaborn-ticks', 'seaborn-white', 'seaborn-whitegrid', 'tableau-colorblind10']
-
 
 def draw_dis(data,tp,sl):
     matplotlib.style.use('bmh')
@@ -148,4 +80,3 @@ def draw_dis(data,tp,sl):
     ax.add_patch(rect)
     plt.show()
 
-#draw_dis(egg,0.7310,0.7290)
--- track_portfolio.py
+++ track_portfolio.py
@@ -1,1 +1,6 @@
 import numpy as np
+
+portfolio_absolute=10000
+trade_amount=portfolio_absolute/100
+
+print('$'+str(portfolio_absolute))
